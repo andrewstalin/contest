2d3e5d3b327e700076a3e4d378eb594f15d257f6
andrew stalin <andrew.stalin@gmail.com>
2024-01-19 09:55:16 +0700

    Time: 14 ms (76.46%), Space: 15.2 MB (35.54%) - LeetHub

---
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int maximumLength(string s) {
+        std::vector<std::vector<int>> data(26, std::vector<int>(51, 0));
+        int k = 1;
+        int count = 1;
+        char ch = s[0];
+        
+        while (k < s.size()) {
+            if (s[k] == ch) {
+                ++count;
+            } else {
+                int index = ch - 'a';
+                
+                for (int i = 1; i <= count; ++i) {
+                    data[index][i] += count - i + 1;
+                }
+                
+                count = 1;
+                ch = s[k];
+            }
+            
+            ++k;
+        }
+        
+        int index = ch - 'a';
+                
+        for (int i = 1; i <= count; ++i) {
+            data[index][i] += count - i + 1;
+        }
+        
+        int res = 0;
+        
+        for (int i = 0; i < 26; ++i) {
+            for (int j = 50; j > 0; --j) {
+                if (data[i][j] >= 3) {
+                    res = std::max(res, j);
+                    break;
+                }
+            }
+        }
+        
+        return res == 0 ? -1 : res;
+    }
+};
