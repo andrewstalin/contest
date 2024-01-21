419f5ea93e6b56ad996c54d3bc76dc3695f2f4c2
andrew stalin <andrew.stalin@gmail.com>
2024-01-21 11:17:56 +0700

    Time: 0 ms (100.00%), Space: 9.2 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,30 +1,6 @@
 class Solution {
 public:
 
-    int func(int pos, const std::vector<int>& nums, std::vector<int>& dp) {
-        if (pos >= nums.size()) {
-            return 0;
-        }
-        
-        if (dp[pos] != -1) {
-            return dp[pos];
-        }
-        
-        dp[pos] = nums[pos] + func(pos + 2, nums, dp);
-        dp[pos] = std::max(dp[pos], func(pos + 1, nums, dp));
-        
-        return dp[pos];
-    }
-    
-    int rob0(vector<int>& nums) {
-        if (nums.size() == 1) {
-            return nums[0];
-        }
-        
-        std::vector<int> dp(nums.size(), -1);
-        return func(0, nums, dp);
-    }
-    
     int rob(vector<int>& nums) {
         if (nums.size() == 1) {
             return nums[0];
