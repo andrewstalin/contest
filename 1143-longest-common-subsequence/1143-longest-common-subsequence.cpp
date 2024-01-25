a487ce765131040210428ed86110cfc77fe13914
andrew stalin <andrew.stalin@gmail.com>
2024-01-25 10:05:31 +0700

    Time: 15 ms (96.96%), Space: 7.7 MB (87.61%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        std::vector<std::vector<int>> dp(text1.size() + 1, std::vector<int>(text2.size() + 1, 0));
+        std::vector<int> dp(text2.size() + 1, 0);
+        int prev, val;
         
         for (int i1 = (text1.size() - 1); i1 >= 0; --i1) {
+            prev = 0;
+            
             for (int i2 = (text2.size() - 1); i2 >= 0; --i2) {
-                dp[i1][i2] = std::max(dp[i1 + 1][i2], dp[i1][i2 + 1]);
+                val = std::max(dp[i2], prev);
                 
                 if (text1[i1] == text2[i2]) {
-                    dp[i1][i2] = std::max(dp[i1][i2], 1 + dp[i1 + 1][i2 + 1]);
+                    val = std::max(val, 1 + dp[i2 + 1]);
                 }
+                
+                dp[i2 + 1] = prev;
+                prev = val;
             }
+            
+            dp[0] = prev;
         }
         
-        return dp[0][0];
+        return dp[0];
     }
 };
