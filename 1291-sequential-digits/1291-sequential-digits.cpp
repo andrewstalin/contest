770ea0cde5223308322a2211d5b67e6d7b356156
andrew stalin <andrew.stalin@gmail.com>
2024-02-02 09:23:56 +0700

    Time: 0 ms (100.00%), Space: 7.4 MB (9.62%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,30 +1,43 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        int head = 1;
+        int tail = 2;
         int val = 12;
         int k = 3;
+        int d = 10;
         
         std::vector<int> res;
         
-        while (head < 9) {
+        while (true) {
             if (val >= low && val <= high) {
                 res.emplace_back(val);
             }
             
             if (k == 10) {
-                ++head;
-                k = head + 2;
-                val = head*10 + (head + 1);
+                ++tail;
+                
+                if (tail == 10) {
+                    break;
+                }
+                
+                val = 1;
+                
+                for (int i = 2; i <= tail; ++i) {
+                    val *= 10;
+                    val += i;
+                }
+                
+                k = tail + 1;
+                d *= 10;
                 continue;
             }
             
+            val %= d;
             val *= 10;
             val += k;
             ++k;
         }
         
-        std::sort(res.begin(), res.end());
         return res;
     }
 };
