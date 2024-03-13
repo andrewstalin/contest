e2d3f6a9f385c9ed9ccdf38bc212451bc9d5bc06
andrew stalin <andrew.stalin@gmail.com>
2024-03-13 10:52:37 +0700

    Time: 3 ms (51.39%), Space: 7.2 MB (41.86%) - LeetHub

---
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int v1 = 0;
+        int v2 = 0;
+        int i1 = 1;
+        int i2 = n;
+        
+        while (i1 < i2) {
+            if (v1 <= v2) {
+                v1 += i1;
+                ++i1;
+            } else {
+                v2 += i2;
+                --i2;
+            }
+        }
+        
+        if (i1 == i2) {
+            v1 += i1;
+            v2 += i2;
+        }
+        
+        return v1 == v2 ? i1 : -1;
+    }
+};
