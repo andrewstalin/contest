6e44708abb2f8536399eda5e16634a28103bafb8
andrew stalin <andrew.stalin@gmail.com>
2024-03-15 10:41:56 +0700

    Time: 21 ms (20.66%), Space: 26.2 MB (86.12%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        std::vector<int64_t> right(nums.size(), 1);
-        int64_t val = 1;
+        std::vector<int> res(nums.size(), 1);
+        int val = 1;
         for (int i = nums.size() - 1; i >= 0; --i) {
-            right[i] = val;
+            res[i] = val;
             val *= nums[i];
         }
         
-        std::vector<int> res(nums.size(), 0);
-        int64_t left = 1;
+        int left = 1;
         for (int i = 0; i < nums.size(); ++i) {
-            res[i] = left*right[i];
+            res[i] *= left;
             left *= nums[i];
         }
         
