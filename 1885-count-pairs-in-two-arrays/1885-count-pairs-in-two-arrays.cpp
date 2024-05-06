a44befb5084219312721f6392612619d0a2ed6b2
andrew stalin <andrew.stalin@gmail.com>
2024-05-06 09:50:57 +0700

    Time: 143 ms (60.14%), Space: 98.8 MB (52.03%) - LeetHub

--- 1885-count-pairs-in-two-arrays/1885-count-pairs-in-two-arrays.cpp
+++ 1885-count-pairs-in-two-arrays/1885-count-pairs-in-two-arrays.cpp
@@ -1,55 +1,29 @@
 class Solution {
 public:
-    // sum on interval [l, end)
-    int query(int r, const std::vector<int>& data) {
-        int res = 0;
-        int l = 0;
-
-        int N = data.size() / 2;
-        l += N;
-        r += N;
-
-        while (l < r) {
-            if (l & 1) {
-                res += data[l++];
-            }
-
-            if (r & 1) {
-                res += data[--r];
-            }
-
-            l >>= 1;
-            r >>= 1;
-        }
-
-        return res;
-    }
-
-    // add 1 at position p
-    void modify(int p, std::vector<int>& data) {
-        p += data.size() / 2;
-        data[p] += 1;
-
-        while (p > 1) {
-            data[p >> 1] = data[p] + data[p ^ 1];
-            p >>= 1;
-        }
-    }
     
     long long countPairs(vector<int>& nums1, vector<int>& nums2) {
-        std::vector<int> data(400004, 0);
-    
-        long long res = 0;
-        modify(nums2.back() - nums1.back() + 100000, data);
+        std::vector<int> diff(nums1.size(), 0);
         
-        int r;
-        for (int i = (nums1.size() - 2); i >= 0; --i) {
-            r = nums1[i] - nums2[i] + 100000;
-            res += query(r, data);
-            modify(nums2[i] - nums1[i] + 100000, data);
+        for (int i = 0; i < nums1.size(); ++i) {
+            diff[i] = nums1[i] - nums2[i];
+        }
+        
+        std::sort(diff.begin(), diff.end());
+        
+        long long sum = 0;
+        int l = 0;
+        int r = diff.size() - 1;
+        
+        while (l < r) {
+            if (diff[l] + diff[r] <= 0) {
+                ++l;
+            } else {
+                sum += (r - l);
+                --r;
+            }
         }
         
         
-        return res;
+        return sum;
     }
 };
