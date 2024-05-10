68b4594de5c5c63f6ab5782e0985b958ac509b8a
andrew stalin <andrew.stalin@gmail.com>
2024-05-10 11:12:55 +0700

    Time: 12 ms (24.47%), Space: 34.1 MB (58.25%) - LeetHub

--- 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.cpp
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
+    int inline max(const std::vector<int>& nums) {
+        int res = nums[0];
+        for (size_t i = 1; i < nums.size(); ++i) {
+            res = std::max(res, nums[i]);
+        }
+        return res;
+    }
     int addedInteger(vector<int>& nums1, vector<int>& nums2) {
-        std::sort(nums1.begin(), nums1.end());
-        std::sort(nums2.begin(), nums2.end());
-        return nums2[0] - nums1[0];
+        return max(nums2) - max(nums1);
     }
 };
