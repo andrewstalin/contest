6b0d06c46fb9bf6bd975ad67e9d1940d3d62e470
andrew stalin <andrew.stalin@gmail.com>
2024-05-18 17:26:44 +0700

    Time: 4 ms (63.21%), Space: 15.6 MB (22.37%) - LeetHub

---
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int func(TreeNode* node, int& res) {
+        if (node->left == nullptr && node->right == nullptr) {
+            res += std::abs(node->val - 1);
+            return node->val - 1;
+        }
+        
+        int val = 0;
+        
+        if (node->left != nullptr){
+            val += func( node->left, res);
+        }
+        
+        if (node->right != nullptr) {
+            val += func(node->right, res);
+        }
+        
+        val += node->val - 1;
+        res += std::abs(val);
+        return val;
+    }
+    
+    int distributeCoins(TreeNode* root) {
+        int res= 0;
+        
+        if (root->left != nullptr) {
+            func(root->left, res);
+        }
+        
+        if (root->right != nullptr) {
+            func(root->right, res);
+        }
+        
+        return res;
+    }
+};
