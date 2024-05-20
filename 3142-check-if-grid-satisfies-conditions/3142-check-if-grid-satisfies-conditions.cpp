439545565db34b673d09b02ee30e9b60a24f6125
andrew stalin <andrew.stalin@gmail.com>
2024-05-20 09:58:59 +0700

    Time: 15 ms (30.62%), Space: 28.5 MB (68.52%) - LeetHub

--- 3142-check-if-grid-satisfies-conditions/3142-check-if-grid-satisfies-conditions.cpp
+++ 3142-check-if-grid-satisfies-conditions/3142-check-if-grid-satisfies-conditions.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     bool satisfiesConditions(vector<vector<int>>& grid) {
-        size_t last_row = grid.size() - 1;
-        size_t last_col = grid[0].size() - 1;
+ 
+        for (size_t col = 0; col < (grid[0].size() - 1); ++col) {
+            if (grid[0][col] == grid[0][col + 1]) {
+                return false;
+            }
+        }
         
-        for (size_t row = 0; row < grid.size(); ++row) {
-            for (size_t col = 0; col < grid[0].size(); ++col) {
-                if ((row != last_row && grid[row][col] != grid[row + 1][col])
-                        || (col != last_col && grid[row][col] == grid[row][col + 1])) {
+        for (size_t col = 0; col < (grid[0].size()); ++col) {
+            int val = grid[0][col];
+            
+            for (size_t row = 1; row < grid.size(); ++row) {
+                if (grid[row][col] != val) {
                     return false;
                 }
-            }
+            } 
         }
         
         return true;
