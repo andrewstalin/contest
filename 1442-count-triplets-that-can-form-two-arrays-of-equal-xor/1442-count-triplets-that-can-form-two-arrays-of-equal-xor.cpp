949f28cb5529fef5ab5a8021df0278594e4fe552
andrew stalin <andrew.stalin@gmail.com>
2024-05-30 10:20:04 +0700

    Time: 4 ms (54.79%), Space: 9.1 MB (67.04%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
-    int countTriplets(vector<int>& arr) {       
+    int countTriplets(vector<int>& arr) { 
+        int res = 0;
+        for (size_t i = 0; i < (arr.size() - 1); ++i) {
+            int val = arr[i];
+
+            for (size_t j = i + 1; j < arr.size(); ++j) {
+                val ^= arr[j];
+                
+                if (val == 0) {
+                    res += (j - i);    
+                }
+            }
+        }
+        return res;
+    }
+    
+    int countTriplets0(vector<int>& arr) {       
         std::vector<std::unordered_map<int, int>> map(arr.size());
         
         for (size_t i = 1; i < arr.size(); ++i) {
