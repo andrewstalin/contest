5587bbe85aa0fa781862ac722b2cf88751322963
andrew stalin <andrew.stalin@gmail.com>
2024-06-06 09:24:56 +0700

    Time: 58 ms (62.52%), Space: 30.5 MB (69.25%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if (hand.size() % groupSize != 0) {
+            return false;
+        }
+        
+        std::map<int, int> map;
+        for (size_t i = 0; i < hand.size(); ++i) {
+            ++map[hand[i]];
+        }
+        
+        auto it = map.begin();
+        
+        while (it != map.end()) {
+            int val = it->first;
+            int count = 1;
+            
+            while (count < groupSize) {
+                ++val;
+                ++count;
+                auto next = map.find(val);
+                
+                if (next == map.end() || next->second < it->second) {
+                    return false;
+                }
+                
+                next->second -= it->second;
+                
+                if (next->second == 0) {
+                    map.erase(next);
+                }
+            }
+            
+            ++it;
+        }
+        
+        return true;
+    }
+};
