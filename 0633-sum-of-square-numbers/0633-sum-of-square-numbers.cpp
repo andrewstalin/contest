2062a32ea1291a2a9f7fadd0bc2c96ce75f46254
andrew stalin <andrew.stalin@gmail.com>
2024-06-17 09:30:40 +0700

    Time: 19 ms (10.70%), Space: 7.1 MB (61.35%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,8 +1,52 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-
+        if (c == 0) {
+            return true;
+        }
         
+        uint32_t end = c;
+        uint32_t k = 1;
+        uint32_t v1 = 1;
+        uint32_t v2;
+        
+        while (v1 <= end) {
+            if (2*v1 == end) {
+                return true;
+            }
+            
+            v2 = end - v1;
+            
+            if (v2 == 0) {
+                return true;
+            }
+            
+            uint32_t l = 0;
+            uint32_t r = std::sqrt(v2) + 1;
+            
+            while (r - l > 1) {
+                uint32_t mid = l + (r - l)/2;
+                uint32_t s = mid*mid;
+                
+                if (s > v2) {
+                    r = mid;
+                } else {
+                    l = mid;
+                }
+            }
+            
+            if (l*l == v2){
+                return true;
+            }
+            
+            ++k;
+            v1 = k*k;
+        }
+        
+        return false;
+    }
+    
+    bool judgeSquareSum0(int c) {
         uint32_t end = c;
         uint32_t k = 0;
         uint32_t v1 = 0;
