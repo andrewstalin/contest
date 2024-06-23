0e7e6e4d83d391903d540ca7a9ba1e9e759294c6
andrew stalin <andrew.stalin@gmail.com>
2024-06-23 13:51:34 +0700

    Time: 79 ms (62.11%), Space: 54.4 MB (81.10%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,6 +1,47 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
+        std::deque<int> max;
+        max.emplace_back(nums[0]);
+        
+        std::deque<int> min;
+        min.emplace_back(nums[0]);
+        
+        int res = 1;
+        int l = 0;
+        int r = 1;
+        
+        while (r < nums.size()) {
+            while (!max.empty() && max.back() < nums[r]) {
+                max.pop_back();
+            }
+            max.emplace_back(nums[r]);
+            
+            while (!min.empty() && min.back() > nums[r]) {
+                min.pop_back();
+            }
+            min.emplace_back(nums[r]);
+            
+            while (std::abs(max.front() - min.front()) > limit) {
+                if (max.front() == nums[l]) {
+                    max.pop_front();
+                }
+                
+                if (min.front() == nums[l]) {
+                    min.pop_front();
+                }
+                
+                ++l;
+            }
+            
+            res = std::max(res, r - l + 1);
+            ++r;
+        }
+        
+        return res;
+    }
+    
+    int longestSubarray0(vector<int>& nums, int limit) {
 
         std::map<int, int> map;
         
