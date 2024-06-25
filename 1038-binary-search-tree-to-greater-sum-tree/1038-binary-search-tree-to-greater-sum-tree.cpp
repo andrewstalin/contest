185feabcf5cd8d02ab596bf441bda10f09b62c58
andrew stalin <andrew.stalin@gmail.com>
2024-06-25 12:50:48 +0700

    Time: 5 ms (15.49%), Space: 10.2 MB (12.64%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -11,8 +11,29 @@
  */
 class Solution {
 public:
+    int walk(int val, TreeNode* node) {
+        int res = 0;
+        
+        if (node->right != nullptr) {
+            res += walk(val, node->right);    
+        }
+        
+        res += node->val;
+        node->val = res + val;
+        
+        if (node->left != nullptr) {
+            res += walk(node->val, node->left);
+        }
+        
+        return res;
+    }
     
     TreeNode* bstToGst(TreeNode* root) {
+        walk(0, root);
+        return root;
+    }
+    
+    TreeNode* bstToGst0(TreeNode* root) {
         int a = 0;
         
         std::stack<std::pair<TreeNode*, bool>> stack;
